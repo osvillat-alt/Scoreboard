8a4540039fd86cdf2970075b871789632ba6f332
osvillat-alt <osvillat@gmail.com>
2025-12-01 16:38:36 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    databaseURL: "TU_DB_URL",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_SENDER",
+    appId: "TU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);

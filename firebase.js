23bee2bd871c7136b579243fd90d71259f5fa02d
osvillat-alt <osvillat@gmail.com>
2025-12-01 17:06:47 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Configuración de tu app web en Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAa5K74St8vyoGt3bttWq0AUlDfdWaBOGs",
+  apiKey: "AIzaSyA...tu clave...",
   authDomain: "scoreboard-62028.firebaseapp.com",
+  databaseURL: "https://scoreboard-62028-default-rtdb.firebaseio.com",
   projectId: "scoreboard-62028",
-  storageBucket: "scoreboard-62028.firebasestorage.app",
+  storageBucket: "scoreboard-62028.appspot.com",
   messagingSenderId: "8415479304",
   appId: "1:8415479304:web:0fe13fd4f35761383133bd"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inicializar Firebase (versión global)
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Realtime Database
+const db = firebase.database();
